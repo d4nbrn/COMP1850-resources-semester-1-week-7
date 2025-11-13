50d3037d18fe5f40ba2a0df1c37f84742545e5cc
d4nbrn <vmzr0073@leeds.ac.uk>
2025-11-13 09:43:43 +0000

    End of task 3

--- week-7-session-2-main/task1/task3.py
+++ week-7-session-2-main/task1/task3.py
@@ -3,6 +3,10 @@ class BankAccount:
         self.account_number = ano
         self.holder_name = name
         self.balance = balance
+    def __str__(self):
+        return f"Bank account {self.account_number} for {self.holder_name} with a balance of {self.balance}"
+    def __repr__(self):
+        return f"BankAccount(ano='{self.account_number}', name='{self.holder_name}', balance={self.balance})"
 
 # Define the __str__ and __repr__ in the following format:
 # (1) for __str__: Bank account 12345678 for John Doe with a balance of 1500.00
