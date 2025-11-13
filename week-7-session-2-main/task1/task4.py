77bc315a080fda3ba81894fe5446ffeebf185e88
d4nbrn <vmzr0073@leeds.ac.uk>
2025-11-13 09:46:30 +0000

    End of task 4

--- week-7-session-2-main/task1/task4.py
+++ week-7-session-2-main/task1/task4.py
@@ -4,6 +4,10 @@ class Transaction:
         self.amount = amt
         self.date = date
         self.description = description
+    def __str__(self):
+        return f"Transaction {transaction_id} on {self.date} for £{self.amount} for {self.description}"
+    def __repr__(self):
+        return f"Transaction(tid='{self.transaction_id}', amt={self.amount}, date='{self.date}', description='{self.description}')"
 
 # define the __str__ and __repr__ in the following format:
 # (1) __str__: Transaction T123 on 2023-11-05 for £500.00 for Rent
