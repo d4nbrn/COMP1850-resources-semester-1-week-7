86f2b2dcb09d3e28f21507cf4f804ecf571cae65
d4nbrn <vmzr0073@leeds.ac.uk>
2025-11-13 09:41:31 +0000

    End of task 2

--- week-7-session-2-main/task1/task2.py
+++ week-7-session-2-main/task1/task2.py
@@ -2,5 +2,8 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
+    def __str__(self):
+        return f"This object is at x-coordinate: {self.x} and y coordinate: {y}"
+    def __repr__(self):
+        return f"x:{self.x} y:{self.y}"
 # Given a Point class, define the __str__ and __repr__ methods
