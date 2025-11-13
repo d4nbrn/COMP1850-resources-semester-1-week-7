0c927e7619e32e222b01e4c09b5083aecaaba399
d4nbrn <vmzr0073@leeds.ac.uk>
2025-11-13 09:31:08 +0000

    End of first task

--- week-7-session-2-main/task1/task1.py
+++ week-7-session-2-main/task1/task1.py
@@ -3,9 +3,14 @@ class Book:
         self.title = title
         self.author = author
         self.year = year
+    def __str__(self):
+        return f"This book is called {self.title}, it is written by {self.author} and published in {self.year}"
+    def __repr__(self):
+        return f"Book: Title = {self.title} Author = {self.author} Year = {self.year}"
 
-
-
+newBook = Book("Harry potter","JK Rowling",1995)
+print(newBook)
+newBook.__repr__()
 # Given a Book class with the __init__ method, define the following special methods:
 # (1) __str__ method - feel free to suggest a suitable string representation
 # (2) __repr__ method - what do you think should be the format?
