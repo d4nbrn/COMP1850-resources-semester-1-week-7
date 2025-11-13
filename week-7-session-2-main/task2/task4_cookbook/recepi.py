4b45f62b8d43b5d3cfa126c4cda9344e67586250
d4nbrn <vmzr0073@leeds.ac.uk>
2025-11-13 09:15:22 +0000

    Add files via upload

---
+++ week-7-session-2-main/task2/task4_cookbook/recepi.py
@@ -0,0 +1,5 @@
+# define a Recepi class representing an individual recepi with fields to storing basic recepi information
+# and methods to get details about the recepi
+
+# if you have completed week-7-session-1/activity2/task5.py, you can copy the code here
+# if not, define the same class here
